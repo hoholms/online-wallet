b375ecfa90f88582ffcae98fae5e545ce4343e7b
”Vetrov <miroslav.vetrov2003@gmail.com>
2022-11-02 13:18:01 +0200

    First commit

---
+++ src/main/java/com/endava_project/media_library/MediaLibraryApplication.java
@@ -0,0 +1,13 @@
+package com.endava_project.media_library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediaLibraryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MediaLibraryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/endava_project/media_library/MediaLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.endava_project.media_library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediaLibraryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
