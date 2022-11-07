3852ffa001a45432491c79976212b051ae924b0c
hoholms <ninikabug@gmail.com>
2022-11-07 23:09:08 +0200

    optimized imports

--- src/main/java/com/endava/wallet/controller/WalletController.java
+++ src/main/java/com/endava/wallet/controller/WalletController.java
@@ -1,6 +1,5 @@
 package com.endava.wallet.controller;
 
-import lombok.Builder;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
