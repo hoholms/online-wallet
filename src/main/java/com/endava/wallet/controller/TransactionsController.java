4c6d2f98ac79e3b527afc3a3e1d3291a435e6fb9
”Vetrov <miroslav.vetrov2003@gmail.com>
2022-11-13 12:17:22 +0200

    transactionEdit view added, TransactionsController new method

--- src/main/java/com/endava/wallet/controller/TransactionsController.java
+++ src/main/java/com/endava/wallet/controller/TransactionsController.java
@@ -1,21 +1,35 @@
 package com.endava.wallet.controller;
 
+import com.endava.wallet.entity.Authority;
+import com.endava.wallet.entity.Transaction;
+import com.endava.wallet.entity.TransactionsCategory;
+import com.endava.wallet.entity.User;
+import com.endava.wallet.repository.TransactionsCategoryRepository;
 import com.endava.wallet.service.TransactionsService;
 import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 @AllArgsConstructor
 public class TransactionsController {
+    private TransactionsCategoryRepository transactionsCategoryRepository;
     private TransactionsService transactionsService;
 
     @GetMapping("/transactions")
     public String getAllTransaction(Model model) {
         model.addAttribute("transactions", transactionsService.getAllTransactions());
         return "transactions";
+    }
+
+    @GetMapping("{transaction}")
+    public String transactionEditForm(@PathVariable Transaction transaction, Model model) {
+        model.addAttribute("transaction", transaction);
+        model.addAttribute("categories", transactionsCategoryRepository.findAll());
+        return "userEdit";
     }
 
 
