eb769a925f04dd9e3212a85f12eb72f1b2d0c519
”Vetrov <miroslav.vetrov2003@gmail.com>
2022-11-13 13:38:26 +0200

    transactionEdit working

--- src/main/java/com/endava/wallet/controller/TransactionController.java
+++ src/main/java/com/endava/wallet/controller/TransactionController.java
@@ -1,31 +1,29 @@
 package com.endava.wallet.controller;
 
-import com.endava.wallet.entity.Authority;
 import com.endava.wallet.entity.Transaction;
-import com.endava.wallet.entity.TransactionsCategory;
-import com.endava.wallet.entity.User;
 import com.endava.wallet.repository.TransactionsCategoryRepository;
 import com.endava.wallet.service.TransactionsService;
 import lombok.AllArgsConstructor;
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @AllArgsConstructor
+@RequestMapping("/transactions")
 public class TransactionController {
     private TransactionsCategoryRepository transactionsCategoryRepository;
     private TransactionsService transactionsService;
 
-    @GetMapping("/transactions")
+    @GetMapping("/")
     public String getAllTransaction(Model model) {
         model.addAttribute("transactions", transactionsService.getAllTransactions());
         return "transactions";
     }
 
-    @GetMapping("{transaction}")
+    @GetMapping("/{transaction}")
     public String transactionEditForm(@PathVariable Transaction transaction, Model model) {
         model.addAttribute("transactionEdit", transaction);
         model.addAttribute("categories", transactionsCategoryRepository.findAll());
