e971aca80b820544ea69c2bc837e3e012f8e100b
Vetrov Miroslav <miroslav.vetrov2003@gmail.com>
2022-11-18 23:45:07 +0200

    Added exception package

---
+++ src/main/java/com/endava/wallet/exception/ApiRequestException.java
@@ -0,0 +1,13 @@
+package com.endava.wallet.exception;
+
+// API Request Exception
+public class ApiRequestException extends RuntimeException{
+
+    public ApiRequestException(String message) {
+        super(message);
+    }
+
+    public ApiRequestException(Throwable cause) {
+        super(cause);
+    }
+}
