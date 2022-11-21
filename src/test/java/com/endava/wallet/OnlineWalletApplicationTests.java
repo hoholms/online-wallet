b48cd5a50104910f5a845e2a051846b8ee4bf87e
”Vetrov <miroslav.vetrov2003@gmail.com>
2022-11-21 13:01:08 +0200

    Application and HttpRequest tests added

--- src/test/java/com/endava/wallet/OnlineWalletApplicationTests.java
+++ src/test/java/com/endava/wallet/OnlineWalletApplicationTests.java
@@ -1,13 +1,32 @@
 package com.endava.wallet;
 
+import com.endava.wallet.controller.*;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @SpringBootTest
 class OnlineWalletApplicationTests {
 
+    @Autowired
+    private DashboardController dashboardController;
+    @Autowired
+    private LoginController loginController;
+    @Autowired
+    private RegisterController registerController;
+    @Autowired
+    private TransactionController transactionController;
+    @Autowired
+    private UserController userController;
     @Test
     void contextLoads() {
+        assertThat(dashboardController).isNotNull();
+        assertThat(loginController).isNotNull();
+        assertThat(registerController).isNotNull();
+        assertThat(transactionController).isNotNull();
+        assertThat(userController).isNotNull();
     }
 
 }
