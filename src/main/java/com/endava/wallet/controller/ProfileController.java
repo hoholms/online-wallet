1cb51ec633403b6f987599ed472df21a9e9036a2
”Vetrov <miroslav.vetrov2003@gmail.com>
2022-11-22 23:21:24 +0200

    updateProfile switch to PostMapping

--- src/main/java/com/endava/wallet/controller/ProfileController.java
+++ src/main/java/com/endava/wallet/controller/ProfileController.java
@@ -11,6 +11,7 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
@@ -35,7 +36,7 @@ public class ProfileController {
         return "profile";
     }
 
-    @GetMapping("profile")
+    @PostMapping("profile")
     public String updateProfile(
             @AuthenticationPrincipal User user,
             @RequestParam String password,
