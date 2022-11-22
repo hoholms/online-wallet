fd513dbccbc34a8c983da0867bf259b0f5fa84e5
hoholms <ninikabug@gmail.com>
2022-11-22 03:31:42 +0200

    Added date form

--- src/main/java/com/endava/wallet/controller/StatisticsController.java
+++ src/main/java/com/endava/wallet/controller/StatisticsController.java
@@ -2,13 +2,18 @@ package com.endava.wallet.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.time.LocalDate;
 
 @Controller
 @RequiredArgsConstructor
 public class StatisticsController {
     @GetMapping("statistics")
-    public String statistics() {
+    public String statistics(Model model) {
+        model.addAttribute("today", LocalDate.now());
+
         return "statistics";
     }
 }
