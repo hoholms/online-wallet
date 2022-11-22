f05a5efe8bcf1365743f21d138849c9bba8a39a5
hoholms <ninikabug@gmail.com>
2022-11-23 01:11:07 +0200

    Added Spring Session JDBC, remember me

--- src/main/java/com/endava/wallet/config/WebSecurityConfig.java
+++ src/main/java/com/endava/wallet/config/WebSecurityConfig.java
@@ -37,6 +37,8 @@ public class WebSecurityConfig {
                         .loginPage("/login")
                         .permitAll()
                 )
+                .rememberMe()
+                .and()
                 .logout(LogoutConfigurer::permitAll);
 
         return http.build();
